b5da6e854b6cc75fa025d1bb7a2d076d2f474394
Thiago Campos <thiago-kono@hotmail.com>
2024-05-27 19:53:26 -0300

    add Controller :   ProdutoController

---
+++ ConsumirAPI/ConsumirAPI/Controllers/ProdutoController.cs
@@ -0,0 +1,23 @@
+﻿using ConsumirAPI.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsumirAPI.Controllers
+{
+    public class ProdutoController : Controller
+    {
+
+        private readonly ApiService _apiService;
+
+        public ProdutoController(ApiService apiService)
+        {
+            _apiService = apiService;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var produtos = await _apiService.GetAllProdutosAsync();
+            return View(produtos);
+        }
+    }
+}
