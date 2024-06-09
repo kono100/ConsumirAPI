425a39eb7b62fc9623ed3de455900df6febc87b4
Thiago Campos <thiago-kono@hotmail.com>
2024-06-09 17:49:35 -0300

    add controller e views dos produtos e corrigido alguns erros

--- ConsumirAPI/ConsumirAPI/Controllers/ProdutoController.cs
+++ ConsumirAPI/ConsumirAPI/Controllers/ProdutoController.cs
@@ -1,12 +1,12 @@
 ﻿using ConsumirAPI.Service;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 using WebAPI_Swagger.Model;
 
 namespace ConsumirAPI.Controllers
 {
     public class ProdutoController : Controller
     {
-
         private readonly ApiService _apiService;
 
         public ProdutoController(ApiService apiService)
@@ -39,6 +39,74 @@ namespace ConsumirAPI.Controllers
                 }
                 ModelState.AddModelError(string.Empty, "Erro ao criar o produto.");
             }
+            return View(produto);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            var produto = await _apiService.GetProdutoAsync(id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            return View(produto);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var produto = await _apiService.GetProdutoAsync(id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            return View(produto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Produto produto)
+        {
+            if (id != produto.Id)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var response = await _apiService.PutProdutoAsync(id, produto);
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                ModelState.AddModelError(string.Empty, "Erro ao atualizar o produto.");
+            }
+            return View(produto);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var produto = await _apiService.GetProdutoAsync(id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            return View(produto);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var response = await _apiService.DeleteProdutoAsync(id);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            ModelState.AddModelError(string.Empty, "Erro ao excluir o produto.");
+            var produto = await _apiService.GetProdutoAsync(id);
             return View(produto);
         }
 
